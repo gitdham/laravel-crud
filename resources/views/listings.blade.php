f6f70e007fd6811282ae093d7677b0abfe2fb146
gitdham <adzani234@gmail.com>
2022-08-30 11:19:06 +0700

    view & layouting

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,12 +1,16 @@
-<h1>{{$heading}}</h1>
+@extends('layout')
 
-@unless (count($listings)<1)
-  @foreach ($listings as $listing)
-    <h2>
-      <a href="/listing/{{$listing['id']}}">{{$listing['title']}}</a>
-    </h2>
-    <p>{{$listing['desc']}}</p>
+@section('content')
+@include('partials._hero')
+@include('partials._search')
+
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+  @unless (count($listings)
+  < 1) @foreach ($listings as $listing) <x-listing-card :listing="$listing" />
   @endforeach
-@else
+  @else
   <p>No listing found</p>
-@endunless
+  @endunless
+</div>
+
+@endsection
