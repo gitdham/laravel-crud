f01f047657cd3688640ee27ba90c3a48eda1f2d3
gitdham <adzani234@gmail.com>
2022-08-30 15:18:51 +0700

    complete crud

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,20 +14,33 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+| Common Resource Routes:
+| index - Show all listings
+| show - Show single listing
+| create - Show form to create new listing
+| store - Store new listing
+| edit - Show form to edit listing
+| update - Update listing
+| destroy - Delete listing
 */
 
 // all listing
-Route::get('/', function () {
-  return view('listings', [
-    'heading' => 'Lastest Listing',
-    'listings' => Listing::all(),
-  ]);
-});
+Route::get('/', [ListingController::class, 'index']);
+
+// show create form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+// store listing data
+Route::post('/listings', [ListingController::class, 'store']);
+
+// show edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// update listing data
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+// destroy listing data
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 // single listing
-Route::get('/listing/{listing}', function (Listing $listing) {
-  return view('listing', [
-    'heading' => 'Selected Listing',
-    'listing' => $listing,
-  ]);
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
