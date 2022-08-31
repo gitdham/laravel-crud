73780b19a10f43a466f42ddeb55b56d234c06883
gitdham <adzani234@gmail.com>
2022-08-31 16:43:07 +0700

    add user_id foreign key

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Listing;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder {
@@ -14,8 +15,15 @@ class DatabaseSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        \App\Models\User::factory(5)->create();
-        Listing::factory(6)->create();
+        // \App\Models\User::factory(5)->create();
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@doe.com'
+        ]);
+
+        Listing::factory(6)->create([
+            'user_id' => $user->id,
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
